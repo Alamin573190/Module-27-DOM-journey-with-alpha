a19b52bb592254eb536795ab69b059075b6a267f
Alamin <mdalaminhossain573190@gmail.com>
2024-02-16 01:56:12 +0600

    score life part

--- js/alpha-class.js
+++ js/alpha-class.js
@@ -7,6 +7,49 @@
 //     const playgroundSection =document.getElementById('play-ground');
 //    playgroundSection.classList.remove('hidden');
 //     };
+function handleKeyboardButtonPress(event){
+    const playerPressed =event.key;
+    console.log(playerPressed);
+
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    // console.log(playerPressed, expectedAlphabet)
+
+    // check matched or not
+    if(playerPressed === expectedAlphabet){
+        console.log('you got a poin')
+        console.log('you have press currectly',expectedAlphabet);
+        // update score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        console.log(currentScore);
+
+        // increase score
+        const newScore = currentScore + 1;
+        currentScoreElement.innerText = newScore;
+
+
+
+        // start a new round
+        removeBackgroundElementById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('right key press');
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        const newLife = currentLife - 1;
+        currentLifeElement.innerText = newLife;
+
+
+    }
+}
+document.addEventListener('keyup', handleKeyboardButtonPress)
 
 function continueGame(){
     // step 1 genarate a random alphabet
--- js/utility.js
+++ js/utility.js
@@ -12,6 +12,10 @@ function setBackgroundElementById(elementId){
     const element =document.getElementById(elementId);
     element.classList.add('bg-orange-400');  
 }
+function removeBackgroundElementById(elementId){
+    const element =document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');  
+}
 
 function getARandonAlphabet(){
     // get or create an alphabet array
